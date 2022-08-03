d78f384c5b7a3392f3f9a52b789dd2b4c07f7b3a
Priscila Cunha <prisciladasilvacunha@gmail.com>
2022-08-02 22:21:58 -0300

    aula4 Estatística Descritiva com R

---
+++ scripts/aula4 Estatística Descritiva com R.R
@@ -0,0 +1,37 @@
+## Estatística Descritiva com R
+# Tabela de Frequencia absoluta da variável Species da base iris
+table(iris$Species)
+
+# Tabela de Frequencia relativa da variável Species da base iris
+prop.table(table(iris$Species))
+
+# Media da variável Sepal.Length da base iris
+mean(iris$Sepal.Length)
+# Mediana da variável Sepal.Length da base iris
+median(iris$Sepal.Length)
+
+# Separatrizes da variável Sepal.Length da base iris
+quantile(iris$Sepal.Length, probs=0.75)
+quantile(iris$Sepal.Length, probs=0.10)
+quantile(iris$Sepal.Length, probs=0.95)
+boxplot(iris$Sepal.Length) #boxplot - gráfico que resume as separatrizes
+
+# Desvio-padrão da variável Sepal.Length da base iris
+sd(iris$Sepal.Length)
+plot(iris$Sepal.Length)
+
+#Sumário descritivo básico das variáveis
+summary(iris)
+
+#Sumário descritivo completo das variáveis usando o pacote fBasics
+pacman::p_load(fBasics)
+basicStats(iris[ , c(1:4)])
+hist(iris$Sepal.Length) #histograma - gráfico que permite conhecer a curva dos dados
+
+
+
+
+
+
+
+
