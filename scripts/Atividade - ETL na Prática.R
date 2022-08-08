d95cbffd5d507664c64e3a6ca6638b933b85bb44
Priscila Cunha <prisciladasilvacunha@gmail.com>
2022-08-08 19:14:15 -0300

    Atividade - ETL na Prática

---
+++ scripts/Atividade - ETL na Prática.R
@@ -0,0 +1,27 @@
+# carrega a base de sinistros de transito do site da PCR
+
+sinistrosRecife2018Raw <- read.csv2('http://dados.recife.pe.gov.br/dataset/44087d2d-73b5-4ab3-9bd8-78da7436eed1/resource/2485590a-3b35-4ad0-b955-8dfc36b61021/download/acidentes_2018.csv', sep = ';', encoding = 'UTF-8')
+
+sinistrosRecife2019Raw <- read.csv2('http://dados.recife.pe.gov.br/dataset/44087d2d-73b5-4ab3-9bd8-78da7436eed1/resource/3531bafe-d47d-415e-b154-a881081ac76c/download/acidentes-2019.csv', sep = ';', encoding = 'UTF-8')
+
+sinistrosRecife2020Raw <- read.csv2('http://dados.recife.pe.gov.br/dataset/44087d2d-73b5-4ab3-9bd8-78da7436eed1/resource/fc1c8460-0406-4fff-b51a-e79205d1f1ab/download/acidentes_2020-novo.csv', sep = ';', encoding = 'UTF-8')
+
+sinistrosRecife2021Raw <- read.csv2('http://dados.recife.pe.gov.br/dataset/44087d2d-73b5-4ab3-9bd8-78da7436eed1/resource/2caa8f41-ccd9-4ea5-906d-f66017d6e107/download/acidentes_2021-jan.csv', sep = ';', encoding = 'UTF-8')
+
+# junta as bases de dados com comando rbind (juntas por linhas)
+
+sinistrosRecifeRaw <- rbind(sinistrosRecife2018Raw, sinistrosRecife2019Raw, sinistrosRecife2020Raw, sinistrosRecife2021Raw)
+
+# observar a estrutura dos dados
+str(sinistrosRecifeRaw)
+
+# modifica a coluna situacao de texto para fator
+sinistrosRecifeRaw$situacao <- as.factor (sinistrosRecifeRaw$situacao)
+
+
+# exporta em formato nativo do R
+saveRDS(sinistrosRecifeRaw, "bases_tratadas/sinistrosRecife.rds")
+
+# exporta em formato tabular (.csv) - padrão para interoperabilidade
+write.csv2(sinistrosRecifeRaw, "bases_tratadas/sinistrosRecife.csv")
+
